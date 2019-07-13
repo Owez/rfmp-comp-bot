56556b20e65fa20fadc50c89c8976a6cfd29820b
scOwez <owez@scalist.net>
2019-07-13 15:48:01 +0100

    Much more to kick command

--- rfmp_comp_bot/cogs/admin.py
+++ rfmp_comp_bot/cogs/admin.py
@@ -22,10 +22,34 @@ class Admin(commands.Cog):
     )
     async def kick_command(self, ctx):
         if ctx.message.author.guild_permissions.administrator:
-            # If user has ban/kick rank
+            if ctx.message.mentions:
+                embed_template = {"main": {}}
 
-            for user_action in ctx.message.mentions:
-                print(user_action.name)
+                for user_action in ctx.message.mentions:
+                    embed_template["main"][f"Kicking {user_action.name}.."] = (
+                        f"Kicking the user named '{user_action.name}' "
+                        f"from {ctx.guild.name}!"
+                    )
+            else:
+                embed_template = {
+                    "main": {
+                        "No users mentioned!": (
+                            "There where no users mentioned in your "
+                            "message so I cannot kick anyone!"
+                        )
+                    }
+                }
+        else:
+            embed_template = {
+                "main": {
+                    "Invalid permissions!": (
+                        "You have not got the sufficant permissions "
+                        f"to use this command, {ctx.message.author.name}"
+                    )
+                }
+            }
+
+        await ctx.send(embed=embed_generator(embed_template))
 
 
 def setup(client):
